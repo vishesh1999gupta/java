d469862464443399a89f353abf94841f9e7b9a90
vishesh gupta <51451155+vishesh1999gupta@users.noreply.github.com>
2019-09-01 21:36:25 +0530

    day3

---
+++ Node.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class Solution {
+
+    public static Node insert(Node head, int data) {
+        //Complete this method
+
+        Node start = head;
+        while (start != null&& start.next!=null) {
+            start = start.next;
+
+        }
+        Node n = new Node(data);
+        if(start!=null)
+        start.next = n;
+        if(head==null)
+            return n;
+        else
+        return head;
+
+
+    }
+
+    public static void display(Node head) {
+        Node start = head;
+        while (start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while (N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head, ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
+
